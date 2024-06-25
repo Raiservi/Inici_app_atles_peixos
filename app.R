a399ce3c92a9b13d43c84eca280d85e8c2d7c919
Raiservi <127210656+Raiservi@users.noreply.github.com>
2024-06-25 19:12:40 +0200

    App definnitiva

--- app.R
+++ app.R
@@ -39,27 +39,23 @@ wms_icgh <- "https://geoserveis.icgc.cat/icc_mapesbase/wms/service"
 
 df_observacions_1 <- read.csv("CSV_observacions/Observacions_peixos_totals.csv", sep = ",", quote = "", header = TRUE, dec = ".", fill = TRUE)
 
-view(df_observacions_1)
+df_observacions_2 <- mutate(df_observacions_1, latitut= as.numeric(X.latitude.),longitut=as.numeric(X.longitude.))
 
-
-df_observacions <- mutate(df_observacions_1, latitut= as.numeric(X.X.latitude..),longitut=as.numeric(X.X.longitude..))
-
-
-
+df_observacions <- df_observacions_2 %>% filter( !is.na(latitut),  !is.na(longitut))
 
 capa_observacions <-  st_as_sf( df_observacions , coords = c("longitut","latitut") )
 
-
 #Carrega de poligons i polilinees
 
-Quadricula_10 <- read_sf("Capes_JSON/UTM1x1costaneres.geojson") %>% st_transform(crs = '+proj=longlat +datum=WGS84')  
+Quadricula_5 <- read_sf("Capes_JSON/UTM1x1_5mts.geojson") %>% st_transform(crs = '+proj=longlat +datum=WGS84')  
+
+Quadricula_10 <- read_sf("Capes_JSON/UTM1x1_10mts.geojson") %>% st_transform(crs = '+proj=longlat +datum=WGS84')  
+
+Quadricula_20 <- read_sf("Capes_JSON/UTM1x1_20mts.geojson") %>% st_transform(crs = '+proj=longlat +datum=WGS84')  
+
+Quadricula_30 <- read_sf("Capes_JSON/UTM1x1_30mts.geojson") %>% st_transform(crs = '+proj=longlat +datum=WGS84')  
 
 Municipis_litorals <- read_sf("Capes_JSON/Municipis_costaners.geojson") %>% st_transform(crs = '+proj=longlat +datum=WGS84')  
-
-
-
-
-
 
 
 ##############################################################################################
@@ -89,14 +85,42 @@ server <- function(input, output) {
     leaflet( ) %>%
       
       #Carrega de la Quadricula 31TDG10 (Color vermell)
+     
+      addPolygons(data = Quadricula_30,stroke = TRUE, fillOpacity = 1,
+                  color = "pink",
+                  weight = 1,
+                  opacity = 0.8,
+                  fill = FALSE,
+                  group = "Quadricula 1x1 fins 30mts"
+      ) %>%
       
+      
+      addPolygons(data = Quadricula_20,stroke = TRUE, fillOpacity = 1,
+                  color = "brown",
+                  weight = 1,
+                  opacity = 0.8,
+                  fill = FALSE,
+                  group = "Quadricula 1x1 fins 20mts"
+      ) %>%
+       
       addPolygons(data = Quadricula_10,stroke = TRUE, fillOpacity = 1,
                   color = "#FF0000",
                   weight = 1,
                   opacity = 0.8,
                   fill = FALSE,
-                  group = "Quadricula sx1"
+                  group = "Quadricula 1x1 fins 5mts"
       ) %>%
+      
+      addPolygons(data = Quadricula_5,stroke = TRUE, fillOpacity = 1,
+                  color = "blue",
+                  weight = 1,
+                  opacity = 0.8,
+                  fill = FALSE,
+                  group = "Quadricula 1x1 fins 10mts"
+      
+      ) %>%
+      
+      
       
       #Carrega de la capa de municipis litorals(lila)
       
@@ -116,26 +140,49 @@ server <- function(input, output) {
               
                  ) %>%
       
-    
-     
+      #Carrega del Mapa base (ortofoto Esri)
       
-      addProviderTiles(providers$Esri.WorldImagery,
-                       group = "Satel.lit ICGC"
+      addProviderTiles(
+        providers$Esri.WorldImagery,
+        group = "Satel.lit ICGC"
       ) %>%
       
       #Carrega del Mapa base ICGC (Mapa base ICGC 1:5000) del serveis WMS
-  
+      
+      addWMSTiles(
+        wms_icgh,
+        layers = "mtc5m",
+        group = "Base ICGC"
+        #options = WMSTileOptions(format = "image/png", transparent = FALSE)
+      ) %>%
+     
+
      
       
       #Posicionament de focus al iniciar el mapa ( centre de la quadricula)
       
-      setView(lng = 1.922069 , lat = 41.633880  , zoom = 7) 
+      setView(lng = 1.922069 , lat = 41.633880  , zoom = 7) %>%
       
       
       # Menu de control de capes afegir/treure i de mapa base ( baseGroups)
       
 
+    addLayersControl(
       
+      baseGroups = c("Satel.lit ICGC","Base ICGC (default)" ),
+      
+      overlayGroups = c( "Municipis litorals",
+                         "Quadricula 1x1 fins 5mts", 
+                         "Quadricula 1x1 fins 10mts", 
+                         "Quadricula 1x1 fins 20mts", 
+                         "Quadricula 1x1 fins 30mts"
+                        
+      ),
+   
+      
+      options = layersControlOptions(collapsed = TRUE)
+      
+    )  
      
       
       
